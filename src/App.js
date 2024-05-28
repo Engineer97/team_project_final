bd4e1da74e4e10fa621c50e7e5722e628b7e0c97
engineer97 <dbgmlwo1997@naver.com>
2024-05-28 16:03:49 +0900

    5월28일 윤사비랑 데이트

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <p>
+          Hello from reacttt
+        </p>
+        <p>
+          나는 강익이 싫어요
+        </p>
+        
+    </div>
+  );
+}
+
+export default App;
